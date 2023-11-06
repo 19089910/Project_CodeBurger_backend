923468e0dd297ebd39925f231cd2baaeb81ffe98
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-11-06 19:56:15 -0400

    fist commit: add all dependencies and all MVC user

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import router from './routes'
+
+import './database'
+
+class App {
+  constructor() {
+    this.app = express()
+    this.middlewares()
+    this.routes()
+  }
+
+  middlewares() {
+    this.app.use(express.json())
+  }
+
+  routes() {
+    this.app.use(router)
+  }
+}
+
+export default new App().app
---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,41 @@
+import User from '../models/User'
+
+import * as Yup from 'yup'
+import { v4 } from 'uuid'
+
+class UserController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().required().min(6),
+      admin: Yup.boolean(),
+    })
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.error })
+    }
+
+    const { name, email, password_hash, admin } = request.body
+
+    const userExists = await User.findOne({
+      where: { email },
+    })
+    if (userExists) {
+      return response.status(400).json({ error: 'User already existe' })
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    })
+
+    return response.status(201).json({ id: user.id, name, email, admin })
+  }
+}
+
+export default new UserController()
