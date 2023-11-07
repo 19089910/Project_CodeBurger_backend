d18eb6eef2f650b76ad31e2d800040f3afbc64b6
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-11-07 19:05:11 -0400

    chore: add multer lib and your config

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import { Router } from 'express'
 
+import multer from 'multer'
+import multerConfig from './config/multer'
+
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import ProductController from './app/controllers/ProductController'
 
+const uploads = multer(multerConfig)
 const router = new Router()
 
 router.post('/users', UserController.store)
 router.post('/sessions', SessionController.store)
-router.post('/products', ProductController.store)
+
+router.post('/products', uploads.single('file'), ProductController.store)
 
 export default router
