863d19a9de1e458911317bc61e0124561ecc5546
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-11-06 20:24:18 -0400

    feat: add lib bcrypt and password-hash in db

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcrypt'
 
 class User extends Model {
   static init(sequelize) {
@@ -7,10 +8,18 @@ class User extends Model {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         admin: Sequelize.BOOLEAN,
       },
       { sequelize },
     )
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 10)
+      }
+    })
+    return this
   }
 }
 
