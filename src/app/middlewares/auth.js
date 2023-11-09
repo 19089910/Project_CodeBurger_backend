7669991d37e50f3a4114330b7de7b109b95b44ee
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-11-08 23:29:06 -0400

    feat: varify if send token and varify cript token is correct

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,6 +1,26 @@
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
+
 export default (request, response, next) => {
-  console.log(request)
   const authToken = request.headers.authorization // the routes I want to block are sending the token this log way
-  console.log(authToken)
+
+  // varify if send token
+  if (!authToken) {
+    return response.status(401).json({ error: 'Token not provided' })
+  }
+
+  const token = authToken.split(' ')[1]
+
+  // varify cript token is correct
+  try {
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+      if (err) throw new Error()
+      console.log(decoded)
+      request.userId = decoded.id
+    })
+  } catch (err) {
+    return response.status(401).json({ error: 'Token is invalid' })
+  }
+
   next()
 }
