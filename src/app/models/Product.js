42c4ae0dfeb0a98bb7d373272f00f699b8f3c2b6
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-11-10 14:22:30 -0400

    fix: associete method was creating a column in the category not in the product

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -18,6 +18,15 @@ class Product extends Model {
     )
     return this // return calss Product to stop here when it needs to and not call associate out of time, category.js also needs to
   }
+
+  // new method that will make the relationship
+  static associate(models) {
+    // creating a new associated column in the product
+    this.belongsTo(models.category, {
+      foreignKey: 'category_id',
+      as: 'category',
+    })
+  }
 }
 
 export default Product
