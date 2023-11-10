43d7c8ced248c562a771bcfd5d1afeb786702470
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-11-10 14:09:38 -0400

    feat: new method that will make the relationship and connect this method in db

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -11,6 +11,15 @@ class Category extends Model {
     )
     return this // return calss Product to stop here when it needs to and not call associate out of time, product.js also needs to
   }
+
+  // new method that will make the relationship
+  static associate(models) {
+    // creating a new associated column in the product
+    this.belongsTo(models.category, {
+      foreignKey: 'category_id',
+      as: 'category',
+    })
+  }
 }
 
 export default Category
