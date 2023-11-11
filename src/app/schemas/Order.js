93ca8b0605e922e2b874a197555493b3bd5bd52d
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2023-11-11 12:42:49 -0400

    feat: schema Order is complite

---
+++ src/app/schemas/Order.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const OrderSchema = new mongoose.Schema(
+  {
+    User: {
+      id: { type: String, requered: true },
+      name: { type: String, requered: true },
+    },
+    products: {
+      id: { type: Number, requered: true },
+      name: { type: String, requered: true },
+      price: { type: Number, requered: true },
+      category: { type: String, requered: true },
+      url: { type: String, requered: true },
+      quantity: { type: String, requered: true },
+    },
+    status: { type: String, requered: true },
+  },
+  { timestamps: true },
+)
+
+export default mongoose.model('Order', OrderSchema)
